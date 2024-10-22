41b32bb93746c78d9be315f9898748cf518e6fea
marcusseixascaf <150820175+marcusseixascaf@users.noreply.github.com>
2024-10-22 14:05:14 -0300

    update cafSolutions import

--- Package.swift
+++ Package.swift
@@ -17,6 +17,10 @@ let package = Package(
             url: "https://github.com/combateafraude/TensorFlowLiteC.git",
             .exact("2.14.1")
         ),
+        .package(
+            url: "https://github.com/combateafraude/CafSolutions.git",
+            .exact("1.0.0")
+        )
     ],
     targets: [
         .binaryTarget(
@@ -24,16 +28,11 @@ let package = Package(
             url: "https://github.com/combateafraude/iOS/releases/download/DocumentDetector-15.0.0/DocumentDetector.xcframework.zip",
             checksum: "b5864f762b50a8e4eab3dfef94740e73590840ad369787a85d8d2208b068f05b"
         ),
-        .binaryTarget(
-            name: "CafSolutions",
-            url: "https://github.com/combateafraude/iOS/releases/download/DocumentDetector-14.0.0/CafSolutions.xcframework.zip",
-            checksum: "f075d5e8e02e4d0120eb7a099f4136d5cf22d1e222ebbfc09985e2b9d00f22dd"
-         ),
         .target(
             name: "DocumentDetectorSDK",
             dependencies: [
                 "DocumentDetector",
-                "CafSolutions",
+                .product(name: "CafSolutions", package: "CafSolutions"),
                 .product(name: "TensorFlowLiteC", package: "TensorFlowLiteC"),
             ],
             path: "./Sources"
